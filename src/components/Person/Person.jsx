c0546deeb2a8a1b7db09c62f2fd25b7ad430099f
Anton Tkachuk <tkacuk2291@gmail.com>
2025-01-05 12:56:30 +0200

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export const Person = ({ person }) => (
-    <>
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && (<p className="Person__age">I am {person.age}</p>)}
-      <p className="Person__partner">{person.isMarried === true ? person.partnerName : 'I am not married'} is my {person.sex === "f" ? 'husband' : 'wife'}</p>
-    </>
-  )
-;
+  <>
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age && <p className="Person__age">I am {person.age}</p>}
+    <p className="Person__partner">
+      {person.isMarried === true ? person.partnerName : 'I am not married'} is
+      my {person.sex === 'f' ? 'husband' : 'wife'}
+    </p>
+  </>
+);
